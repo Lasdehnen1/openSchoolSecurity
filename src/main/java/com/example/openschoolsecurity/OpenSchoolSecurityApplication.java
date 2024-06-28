0836765d0a604ac3e3377d832e3a0a308f2cd172
Evgenii.Pichugov <eugepic@yandex.ru>
2024-06-28 12:55:40 +0300

    initial commit

---
+++ src/main/java/com/example/openschoolsecurity/OpenSchoolSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.openschoolsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSchoolSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenSchoolSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/openschoolsecurity/OpenSchoolSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openschoolsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSchoolSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
